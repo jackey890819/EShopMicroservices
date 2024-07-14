619eef8dbc7ea9025d535c4c2f753209b031f42c
Shun-Fa Yang <jackey890819@gmail.com>
2024-07-14 23:50:21 +0800

    feat: Chapter9實作

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,32 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 // add service to the container.
+
+System.Reflection.Assembly programAssembly = typeof(Program).Assembly;
+builder.Services.AddCarterWithAssemblies(programAssembly);
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(programAssembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 // configure the http request pipeline
 
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 app.Run();
